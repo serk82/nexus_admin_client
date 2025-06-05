f0e437eafc1971e2b780ac47f3ef745e3feaf818
serk82 <serk82@gmail.com>
2025-06-05 14:12:28 +0200

    Refactor: agrega gestión de caducidad de tacógrafo y reorganiza la interfaz de expiraciones en el formulario de vehículos

--- views/forms/notifications.py
+++ views/forms/notifications.py
@@ -34,7 +34,11 @@ class frm_notifications(QDialog):
         self.user = self.users_controller.get_user_by_id(
             self.auth_manager.token, self.auth_manager.user_id
         )
+        print(self.user)
         self.user_notification_itv_expiry = self.user.get("notification_itv_expiry")
+        self.user_notification_tachograph_expiry = self.user.get(
+            "notification_tachograph_expiry"
+        )
         self.user_notification_inspection_kms_expiry = self.user.get(
             "notification_inspection_kms_expiry"
         )
@@ -95,12 +99,60 @@ class frm_notifications(QDialog):
             itv_item = self.check_itv(vehicle)
             if itv_item:
                 vehicle_item.appendRow(itv_item)
+            tachograph_item = self.check_tachograph(vehicle)
+            if tachograph_item:
+                vehicle_item.appendRow(tachograph_item)
             inspection_kms_item = self.check_inspection(vehicle)
             if inspection_kms_item:
                 vehicle_item.appendRow(inspection_kms_item)
             if vehicle_item.rowCount() > 0:
                 vehicle_items.append(vehicle_item)
         return vehicle_items
+
+    def check_inspection(self, vehicle):
+        if not vehicle.get("deactivate"):
+            if (
+                self.user_notification_inspection_kms_expiry is not None
+                and vehicle.get("inspection_km") is not None
+            ):
+                current_kms = None
+                for current_kms_vehicle in self.all_current_kms.findall("GRUA"):
+                    if current_kms_vehicle.find("MATRICULA").text == vehicle.get(
+                        "license_plate"
+                    ):
+                        current_kms = current_kms_vehicle.find("KMS").text
+                        break
+                next_inspection = self.get_next_kms_inspection(
+                    vehicle.get("id"), vehicle.get("inspection_km")
+                )
+                if current_kms and next_inspection:
+                    current_kms = int(current_kms)
+                    kms_remainig = next_inspection - current_kms
+                    if kms_remainig <= self.user_notification_inspection_kms_expiry:
+                        inspection_kms_item = QStandardItem(
+                            f"Revisión a los {get_format_miles(next_inspection)} KMS"
+                        )
+                        kms_remainig_item = QStandardItem()
+                        if kms_remainig < 0:
+                            kms_remainig_item.setForeground(QColor("red"))
+                            kms_remainig_item.setText(
+                                f"!!! Revisión Caducada !!! han pasado {get_format_miles(-kms_remainig)} KMS"
+                            )
+                        elif kms_remainig < 100 and kms_remainig >= 0:
+                            kms_remainig_item.setForeground(QColor("orange"))
+                            kms_remainig_item.setText(
+                                f"Revisión a punto de caducar. Quedan {get_format_miles(kms_remainig)} KMS"
+                            )
+                        else:
+                            kms_remainig_item.setForeground(QColor("green"))
+                            kms_remainig_item.setText(
+                                f"Quedan {get_format_miles(kms_remainig)} KMS"
+                            )
+                        return [
+                            inspection_kms_item,
+                            kms_remainig_item,
+                        ]
+        return None
 
     def check_itv(self, vehicle):
         if not vehicle.get("deactivate"):
@@ -137,47 +189,43 @@ class frm_notifications(QDialog):
                     ]
         return None
 
-    def check_inspection(self, vehicle):
+    def check_tachograph(self, vehicle):
         if not vehicle.get("deactivate"):
+            print(self.user_notification_tachograph_expiry)
+            print(vehicle.get("tachograph_expiry"))
             if (
-                self.user_notification_inspection_kms_expiry is not None
-                and vehicle.get("inspection_km") is not None
+                self.user_notification_tachograph_expiry is not None
+                and vehicle.get("tachograph_expiry") is not None
             ):
-                current_kms = None
-                for current_kms_vehicle in self.all_current_kms.findall("GRUA"):
-                    if current_kms_vehicle.find("MATRICULA").text == vehicle.get("license_plate"):
-                        current_kms = current_kms_vehicle.find("KMS").text
-                        break
-                next_inspection = self.get_next_kms_inspection(
-                    vehicle.get("id"), vehicle.get("inspection_km")
+                target_date = datetime.strptime(
+                    vehicle.get("tachograph_expiry"), "%Y-%m-%d"
                 )
-                if current_kms and next_inspection:
-                    current_kms = int(current_kms)
-                    kms_remainig = next_inspection - current_kms
-                    if kms_remainig <= self.user_notification_inspection_kms_expiry:
-                        inspection_kms_item = QStandardItem(
-                            f"Revisión a los {get_format_miles(next_inspection)} KMS"
+                limit_date = target_date.date() - timedelta(
+                    days=self.user_notification_tachograph_expiry
+                )
+                if self.today >= limit_date:
+                    days_remainig = target_date.date() - self.today
+                    tachograph_expiry_item = QStandardItem(
+                        f"Caducidad Tacógrafo: {target_date.date().strftime('%d/%m/%Y')}"
+                    )
+                    days_remainig_item = QStandardItem()
+                    if days_remainig.days < 0:
+                        days_remainig_item.setForeground(QColor("red"))
+                        days_remainig_item.setText(
+                            f"!!! Tacógrafo Caducado !!! han pasado {days_remainig.days} días"
                         )
-                        kms_remainig_item = QStandardItem()
-                        if kms_remainig < 0:
-                            kms_remainig_item.setForeground(QColor("red"))
-                            kms_remainig_item.setText(
-                                f"!!! Revisión Caducada !!! han pasado {get_format_miles(-kms_remainig)} KMS"
-                            )
-                        elif kms_remainig < 100 and kms_remainig >= 0:
-                            kms_remainig_item.setForeground(QColor("orange"))
-                            kms_remainig_item.setText(
-                                f"Revisión a punto de caducar. Quedan {get_format_miles(kms_remainig)} KMS"
-                            )
-                        else:
-                            kms_remainig_item.setForeground(QColor("green"))
-                            kms_remainig_item.setText(
-                                f"Quedan {get_format_miles(kms_remainig)} KMS"
-                            )
-                        return [
-                            inspection_kms_item,
-                            kms_remainig_item,
-                        ]
+                    elif days_remainig.days < 10 and days_remainig.days >= 0:
+                        days_remainig_item.setForeground(QColor("orange"))
+                        days_remainig_item.setText(
+                            f"Tacógrafo a punto de caducar. Quedan {days_remainig.days} días"
+                        )
+                    else:
+                        days_remainig_item.setForeground(QColor("green"))
+                        days_remainig_item.setText(f"Quedan {days_remainig.days} días")
+                    return [
+                        tachograph_expiry_item,
+                        days_remainig_item,
+                    ]
         return None
 
     def get_next_kms_inspection(self, vehicle_id, target_kms):
