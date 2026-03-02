9a029bcff6d54629a236e4f2cd0870d2e19bb418
serk82 <serk82@gmail.com>
2026-03-02 13:23:20 +0100

    Refactor: elimina el código comentado relacionado con la verificación de actualizaciones y el inicio de sesión en la clase app

--- app.py
+++ app.py
@@ -25,10 +25,37 @@ if __name__ == "__main__":
         app.setStyleSheet(file.read())
 
     # Check for updates
-    update_form = frm_update()
-    if update_form.check_update():
-        update_form.exec()
-    login = frm_login()
-    login.show()
+    # update_form = frm_update()
+    # if update_form.check_update():
+    #     update_form.exec()
+    # login = frm_login()
+    # login.show()
+
+    auth_manager = AuthManager()
+    if auth_manager.login("admin", "69ADM82s@"):
+        # if auth_manager.login("carles", "1981"):
+
+        form = frm_notifications(None, auth_manager, 1)
+        form.exec()
+        form = frm_options(auth_manager)
+        form.show()
+
+        # form = frm_configuration(auth_manager)
+        # form.show()
+
+        # form = frm_main(auth_manager, 4)
+        # form.show()
+
+        # form = frm_permissions(None, auth_manager)
+        # form.show()
+
+        # form = frm_table_view(None, auth_manager, "companies", 1)
+        # form.show()
+
+        # form = frm_vehicles(None, auth_manager, 4)
+        # form.show()
+
+        # form = frm_vehicle(None, auth_manager, True, 161, 4)
+        # form.show()
 
     sys.exit(app.exec())
